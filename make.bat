cd0d3a5083488fc8e2301f9fac418ca783192800
4337 <tao.ceee.tung@gmail.com>
2016-03-11 11:27:48 +0100

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 @echo off
 
 C:\masm32\bin\ml.exe /c /coff /I C:\masm32 loader.asm
-C:\masm32\bin\link.exe /SUBSYSTEM:CONSOLE /LIBPATH:C:\masm32\lib /BASE:0x900000 /OUT:loader.exe loader.obj
+C:\masm32\bin\link.exe /SUBSYSTEM:CONSOLE /LIBPATH:C:\masm32\lib /OUT:loader.exe loader.obj
 
 del loader.obj
 
